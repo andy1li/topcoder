f3a489dbd205ed50e3063492ed8b840e21221507
andy1li <li.chenxing@gmail.com>
2016-04-11 02:21:04 +0800

    Add 250 Substitute

---
+++ 250 Substitute.py
@@ -0,0 +1,42 @@
+# https://arena.topcoder.com/#/u/practiceCode/1282/1262/1333/2/1282
+
+class Substitute(object):
+    def substitute(self, c):
+        d = self.key.find(c)+1
+        print c, self.key, d
+        if d < 1:
+            d = '-'
+        if d == len(self.key):
+            d = 0
+            
+        return str(d)
+        
+    def getValue(self, key, code):
+        self.key = key
+        result = map(self.substitute, code)
+        result = filter(lambda x: x != '-', result)
+        
+        return int(''.join(result))
+
+sub = Substitute()
+print sub.getValue("TRADINGFEW", "LGXWEV")
+
+'''
+Examples
+0)
+"TRADINGFEW"
+"LGXWEV"
+Returns: 709
+The L,X, and V are ignored since they do not appear in the key. 
+G is the seventh letter in the key, W is the 10th letter, and E is the 9th letter.
+
+1)
+"ABCDEFGHIJ"
+"XJ"
+Returns: 0
+
+2)
+"CRYSTALBUM"
+"MMA"
+Returns: 6
+'''
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# topcoder
-My solutions to Topcoder practice problems
+# Gists of Topcoder practice problems
+
+#### [250 Substitute](https://arena.topcoder.com/#/u/practiceCode/1282/1262/1333/2/1282)
+>`【Trivial】Use map() to substitute the values in the code`
